31ba29bb9a007cb15a515bc84c8f3c2f7c9b0c32
Vlad <vlad.iliushin@gmail.com>
2012-09-30 15:39:25 +0200

    First task

---
+++ vectors.py
@@ -0,0 +1,11 @@
+class MyVector:
+	def __init__(self,vector=None): 
+		self.vector=vector if vector else [] #Some tutorial (No human factor errors)
+
+	def get_vector(self):
+		return(self.vector)		
+	
+#Skalarni soucin	
+	def __mul__(self,other):
+		dot=sum(p*q for p,q in zip(self.vector, other.vector))
+		return(dot)
