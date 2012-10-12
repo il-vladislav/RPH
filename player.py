db291c1892e6620e24e1570d4b45676ca04e6e08
Vlad <vlad.iliushin@gmail.com>
2012-10-12 13:03:31 +0200

    1

--- player.py
+++ player.py
@@ -1,29 +1,30 @@
 import random
 class MyPlayer:
-    """good"""
+    """tit for tat with forgiveness"""
     def __init__(self,payoff_matrix, number_of_iterations=0):
         self.payoff_matrix=payoff_matrix
         self.number_of_iterations=number_of_iterations
-        self.opponent_move=[]
-        self.next_move=self.opponent_move
-        self.movecounter=0
         self.badmoves=0
+        self.movecounter=0 #for future
         
     def record_opponents_move(self,opponent_move):
-        self.next_move=opponent_move
+        self.opponent_move=opponent_move
         
     def move(self):
+        #Prvni tah je Cooperate
         if (self.movecounter==0):
-            self.movecounter=1
-            return True
-        #bad_opponent_move_counter()
-        if (self.badmoves==6):
-            return True		
-        return self.next_move
-        
-    def bad_opponent_move_counter(self):
-        if (self.next_move==True):
+            self.movecounter=self.movecounter+1
+            return False
+        #Scitame, kolik opponent hral DEFECT po sobe
+        if (self.opponent_move==True):
             self.badmoves=self.badmoves+1
-        if (self.next_move==False):
+        if (self.opponent_move==False):
             self.badmoves=0
+        #Anticyklacni DEFECT
+        if (self.badmoves==6):
+            self.movecounter=self.movecounter+1
+            return False
+        #Tii-for-tat
+        self.movecounter=self.movecounter+1
+        return self.opponent_move
         
