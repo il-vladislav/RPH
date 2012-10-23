2b17ad86905cd9f35141198878848dcfad2eb776
Vlad <vlad.iliushin@gmail.com>
2012-10-23 16:57:17 +0200

    SPAM krok 3-6 (still in progress)

---
+++ filter.py
@@ -0,0 +1,7 @@
+class MyFilter:
+        def __init__(self):
+
+        def train(path_to_truth_dir):
+
+        def test(path_to_pred_dir):
+                
---
+++ trainingcorpus.py
@@ -0,0 +1,16 @@
+class TrainingCorpus:
+        def __init__(self):
+
+        def get_class(email_name):
+                return 'OK'
+
+        def is_ham(email_name):
+                return False
+
+        def is_spam(email_name):
+                return False
+
+        def spams_as_string():
+
+        def hams_as_string():
+                
