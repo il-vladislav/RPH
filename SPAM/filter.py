af1f2ec32d0c791c796010c2b60de749ffe7165a
Vlad <vlad.iliushin@gmail.com>
2012-11-02 21:41:30 +0100

    Filter progress

--- SPAM/filter.py
+++ SPAM/filter.py
@@ -14,28 +14,7 @@ class MyFilter:
                 
 
         def train(self,path_to_truth_dir):
-                from_dict = {}
-                spamers = {}
-                hamers = {}
-                prediction = utils.read_classification_from_file(path_to_truth_dir, "!truth.txt")
-                corpus = Corpus(path_to_truth_dir)
-                for fname, body in corpus.emails_as_string():
-                        email_file = open(corpus.add_slash(path_to_truth_dir)+fname,'r')
-                        msg = email.message_from_file(email_file)
-                        from_dict[self.extract_email_adress_from_text(msg['From'])] = prediction[fname]
-                for i in from_dict:
-                        if (from_dict[i] == 'SPAM'):
-                                try:
-                                        spamers[i] += 1
-                                except KeyError:
-                                        spamers[i] = 1
-                        if (from_dict[i] == 'OK'):
-                                try:
-                                        hamers[i] += 1
-                                except KeyError:
-                                        hamers[i] = 1
-                self.spamers = spamers
-                self.hamers = hamers
+                self.FROM_emails_to_dict(path_to_truth_dir)
 
         def test(self, path_to_test_dir):
                 prediction_dict = {}
@@ -49,26 +28,57 @@ class MyFilter:
                                 prediction_dict[fname] = 'OK'
                 bf1 = BaseFilter(path_to_test_dir,prediction_dict)
                 bf1.generate_prediction_file()
+
+
+        
+        def FROM_emails_to_dict(self,path):
+                #hamers and spamers are dict with type 'str':'str'
+                #fname : FROM_email
+                #'1145.34a6c095d84e586da2b0177b7914882f' : 'robinderbains@shaw.ca'
+                spamers = {}
+                hamers = {}
                 
+                prediction = utils.read_classification_from_file(path, "!truth.txt")
+                corpus = Corpus(path)
+                
+                for fname, body in corpus.emails_as_string():
+                        email_file = open(corpus.add_slash(path)+fname,'r')
+                        msg = email.message_from_file(email_file)
+                        i = self.extract_email_adress_from_text(msg['From'])
+                        if (prediction[fname] == 'SPAM'):
+                                spamers[i] = fname
+                        elif (prediction[fname] == 'OK'):
+                                hamers[i] = fname
+                self.spamers = spamers
+                self.hamers = hamers
+                
+                self.generate_file_from_dict(path,'!spammers.txt', spamers)
+                self.generate_file_from_dict(path,'!hamers.txt',hamers)
 
         def extract_email_adress_from_text(self, text):
+                #input "Monty Solomon <monty@roscom.com>"
+                #output "monty@roscom.com"
                 mailsrch = re.compile(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}')
                 list_of_emails = mailsrch.findall(text)
                 if not list_of_emails:
                         return "None"
                 return list_of_emails[0]
 
-        def generate_spamers_file(self,path_to_test_dir):
-                path_to_prediction_file = self.add_slash(path_to_test_dir)+'!spamers.txt'
+        def generate_file_from_dict(self,path, fname, my_dict):
+                #input path, output filename and dict
+                #output file path/filename with dict
+                #C:/1/!spamers.txt with dict of spamers
+                path_to_prediction_file = self.add_slash(path)+fname
                 prediction_file = open(path_to_prediction_file, 'w+')
-                pickle.dump(self.spamers, prediction_file)
+                pickle.dump(my_dict, prediction_file)
                 prediction_file.close()
 
-        def read_spamers_file(self, path_to_test_dir):
-                pkl_file = open(self.add_slash(path_to_test_dir)+'!spamers.txt')
-                mydict2 = pickle.load(pkl_file)
+        def read_dict_from_file(self, path, fname):
+                #reverse operation
+                pkl_file = open(self.add_slash(path)+fname)
+                my_dict = pickle.load(pkl_file)
                 pkl_file.close()
-                return mydict2
+                return my_dict
                 
         def add_slash(self, path):
                 if path.endswith("/"): return path
