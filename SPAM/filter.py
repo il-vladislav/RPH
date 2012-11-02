c31ab4df8236f97ebbbb95b5271c0491a4339137
Vlad <vlad.iliushin@gmail.com>
2012-11-02 15:16:59 +0100

    Test,train

--- SPAM/filter.py
+++ SPAM/filter.py
@@ -2,46 +2,52 @@ import utils
 import email
 import Corpus
 import re
+import pickle
 from Corpus import Corpus
 import basefilter
 from basefilter import BaseFilter
 
 class MyFilter:
-        def __init__(self,path_to_truth_dir):
-                self.path_to_truth_dir = path_to_truth_dir if path_to_truth_dir else []
+        def __init__(self):
                 self.spamers = {}
                 self.hamers = {}
                 
 
-        def train(self):
+        def train(self,path_to_truth_dir):
                 from_dict = {}
                 spamers = {}
                 hamers = {}
-                prediction = utils.read_classification_from_file(self.path_to_truth_dir, "!truth.txt")
-                corpus = Corpus(self.path_to_truth_dir)
+                prediction = utils.read_classification_from_file(path_to_truth_dir, "!truth.txt")
+                corpus = Corpus(path_to_truth_dir)
                 for fname, body in corpus.emails_as_string():
-                        email_file = open(corpus.add_slash(self.path_to_truth_dir)+fname,'r')
+                        email_file = open(corpus.add_slash(path_to_truth_dir)+fname,'r')
                         msg = email.message_from_file(email_file)
                         from_dict[self.extract_email_adress_from_text(msg['From'])] = prediction[fname]
                 for i in from_dict:
                         if (from_dict[i] == 'SPAM'):
-                                spamers[i] = 1
+                                try:
+                                        spamers[i] += 1
+                                except KeyError:
+                                        spamers[i] = 1
                         if (from_dict[i] == 'OK'):
-                                hamers[i] = 1
+                                try:
+                                        hamers[i] += 1
+                                except KeyError:
+                                        hamers[i] = 1
                 self.spamers = spamers
                 self.hamers = hamers
 
-        def test(self, path):
+        def test(self, path_to_test_dir):
                 prediction_dict = {}
-                corpus = Corpus(path)
+                corpus = Corpus(path_to_test_dir)
                 for fname, body in corpus.emails_as_string():
-                        email_file = open(corpus.add_slash(path)+fname,'r')
+                        email_file = open(corpus.add_slash(path_to_test_dir)+fname,'r')
                         msg = email.message_from_file(email_file)
                         if(self.extract_email_adress_from_text(msg['From']) in self.spamers):
                                 prediction_dict[fname] = 'SPAM'
                         else:
                                 prediction_dict[fname] = 'OK'
-                bf1 = BaseFilter(path,prediction_dict)
+                bf1 = BaseFilter(path_to_test_dir,prediction_dict)
                 bf1.generate_prediction_file()
                 
 
@@ -51,6 +57,23 @@ class MyFilter:
                 if not list_of_emails:
                         return "None"
                 return list_of_emails[0]
+
+        def generate_spamers_file(self,path_to_test_dir):
+                path_to_prediction_file = self.add_slash(path_to_test_dir)+'!spamers.txt'
+                prediction_file = open(path_to_prediction_file, 'w+')
+                pickle.dump(self.spamers, prediction_file)
+                prediction_file.close()
+
+        def read_spamers_file(self, path_to_test_dir):
+                pkl_file = open(self.add_slash(path_to_test_dir)+'!spamers.txt')
+                mydict2 = pickle.load(pkl_file)
+                pkl_file.close()
+                return mydict2
+                
+        def add_slash(self, path):
+                if path.endswith("/"): return path
+                return path + "/"
+
                 
                         
                         
--- SPAM/quality.py
+++ SPAM/quality.py
@@ -5,7 +5,7 @@ import utils
 def quality_score(tp, tn, fp, fn):       
         return (tp+tn)/(tp+tn+10*fp+fn)
 
-def compute_quality_for_corpus(corpus_dir):
+def compute_quality_for_corpus(corpus_dir ):
         truth_dic = utils.read_classification_from_file(corpus_dir, "!truth.txt")
         pred_dic = utils.read_classification_from_file(corpus_dir, "!prediction.txt")
         bc1 = BinaryConfusionMatrix('SPAM', 'OK')
@@ -15,4 +15,4 @@ def compute_quality_for_corpus(corpus_dir):
         tn=dict_score['tn']
         fp=dict_score['fp']
         tp=dict_score['tp']
-        return quality_score(tp, tn, fp, fn)
+        return quality_score(tp, tn, fp, fn), tp, tn, fp, fn
