5bea9b8a2baa777ddb8e0ebbdcf8a521534aa044
Vlad <vlad.iliushin@gmail.com>
2012-12-27 01:56:06 +0100

    Spam migration

--- SPAM/tokenizer.py
+++
@@ -1,9 +0,0 @@
-##########################################
-#Python 3 ONLY
-##########################################
-def shortphrase(shortphrase):
-        if type(shortphrase) == str:
-                shortphrase = shortphrase.translate(str.maketrans('.',' '))
-                tokens = shortphrase.lower().split()
-                return tokens
-        return ""
